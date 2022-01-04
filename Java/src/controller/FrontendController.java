cbc09f4592d8e57efdf7e55f9cd9683da97de435
John Stone <john@jastec.co.uk>
2022-01-04 09:46:28 +0000

    database sample data

--- Java/src/controller/FrontendController.java
+++ Java/src/controller/FrontendController.java
@@ -15,39 +15,55 @@ public class FrontendController {
         User user = new User(bController.createNewUser());
         ArrayList<Performance> results = new ArrayList<Performance>();
         
-        // results = bController.getShowsFromTitle("La");
-        // results = bController.getAllShows();
+        //results = bController.getShowsFromTitle("Oliver!");
+        //results = bController.getAllShows();
         // results = bController.getShowsFromType("Theat");
         // results = bController.getShowsFromTime("evening");
         // results = bController.getShowsFromMaxDuration(140);
-        results = bController.getShowsFromDate(2022, 01, 03);
+        results = bController.getShowsFromDate(2022, 01, 01);
         System.out.println("USER ID");
         System.out.println(user.getUserID());
         System.out.println("---------");
 
-        for (int i = 0; i < results.size(); i++) {
-            System.out.println("Title");
-            System.out.println(results.get(i).getTitle());
-            System.out.println("---------");
-            System.out.println("Type");
-            System.out.println(results.get(i).getType());
-            System.out.println("---------");
-            System.out.println("Language");
-            System.out.println(results.get(i).getType().getLanguage());
-            System.out.println("---------");
-            System.out.println("Date");
-            System.out.println(results.get(i).getDate());
-            System.out.println("---------");
-            System.out.println("Time");
-            System.out.println(results.get(i).getTime());
-            System.out.println("---------");
-            System.out.println("Performers");
-            for (int j = 0; j < results.get(i).getType().getPerformers().size(); j++) {
-                System.out.println(results.get(i).getType().getPerformers().get(j).getName());
-            }
-            System.out.println("---------");
-            System.out.println("---------");
-            System.out.println("---------");
+        
+        System.out.println(results);
+        for (Performance p : results) {
+     
+        	System.out.print(
+        					p.getPerformanceID() +" "
+        					+ p.getDate() + " "
+        					+ p.getTime() + " "
+        					+ p.getTitle() + " "
+        					+ p.getType() + " "
+        					+ p.getStallsAvailable()
+        					+  "\n");
         }
+        
+        
+        
+//        for (int i = 0; i < results.size(); i++) {
+//            System.out.println("Title");
+//            System.out.println(results.get(i).getTitle());
+//            System.out.println("---------");
+//            System.out.println("Type");
+//            System.out.println(results.get(i).getType());
+//            System.out.println("---------");
+//            System.out.println("Language");
+//            System.out.println(results.get(i).getType().getLanguage());
+//            System.out.println("---------");
+//            System.out.println("Date");
+//            System.out.println(results.get(i).getDate());
+//            System.out.println("---------");
+//            System.out.println("Time");
+//            System.out.println(results.get(i).getTime());
+//            System.out.println("---------");
+//            System.out.println("Performers");
+//            for (int j = 0; j < results.get(i).getType().getPerformers().size(); j++) {
+//                System.out.println(results.get(i).getType().getPerformers().get(j).getName());
+//            }
+//            System.out.println("---------");
+//            System.out.println("---------");
+//            System.out.println("---------");
+//        }
     }
 }
