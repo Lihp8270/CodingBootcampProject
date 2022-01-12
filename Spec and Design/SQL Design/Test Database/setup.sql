cc116ff91bf050bfa41754210c4ae00501352dfa
John Stone <john@jastec.co.uk>
2022-01-12 13:56:11 +0000

    added all in one setup.sql file

---
+++ Spec and Design/SQL Design/Test Database/setup.sql
@@ -0,0 +1,377 @@
+-- Create DB
+DROP database IF EXISTS theatre ;
+
+CREATE database IF NOT EXISTS theatre;
+USE theatre
+;
+
+-- Customer table
+DROP TABLE IF EXISTS theatre.customer ;
+
+CREATE TABLE IF NOT EXISTS theatre.customer (
+  id INT NOT NULL AUTO_INCREMENT,
+  customer_name VARCHAR(45),
+  customer_email VARCHAR(45),
+  customer_password VARCHAR(45),
+  address_line_1 VARCHAR(45),
+  address_line_2 VARCHAR(45),
+  postcode VARCHAR(45),
+  temp_status boolean NOT NULL DEFAULT 1,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id))
+;
+
+
+-- Seat table
+DROP TABLE IF EXISTS theatre.seat ;
+
+CREATE TABLE IF NOT EXISTS theatre.seat (
+  id INT NOT NULL,
+  location ENUM('stalls', 'circle') NOT NULL,
+  PRIMARY KEY (id))
+;
+
+
+-- Performer table
+DROP TABLE IF EXISTS theatre.performer ;
+
+CREATE TABLE IF NOT EXISTS theatre.performer (
+  id INT NOT NULL AUTO_INCREMENT,
+  performer_name VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id, performer_name))
+;
+
+
+-- production_category table
+DROP TABLE IF EXISTS theatre.production_category ;
+
+CREATE TABLE IF NOT EXISTS theatre.production_category (
+  id INT NOT NULL AUTO_INCREMENT,
+  category_name VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX name_UNIQUE (category_name ASC))
+;
+
+
+-- Concession table
+DROP TABLE IF EXISTS theatre.concession ;
+
+CREATE TABLE IF NOT EXISTS theatre.concession (
+  id INT NOT NULL AUTO_INCREMENT,
+  concession_name VARCHAR(45) NOT NULL,
+  discount FLOAT NOT NULL,
+  PRIMARY KEY (id))
+;
+
+
+-- production table
+DROP TABLE IF EXISTS theatre.production ;
+
+CREATE TABLE IF NOT EXISTS theatre.production (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NOT NULL,
+  production_description VARCHAR(250) NULL,
+  duration INT NOT NULL,
+  category_id INT NOT NULL,
+  price DECIMAL(5,2) NOT NULL,
+  production_language VARCHAR(45) NULL,
+  PRIMARY KEY (id),
+  INDEX category_id_idx (category_id ASC) ,
+  CONSTRAINT category_id
+    FOREIGN KEY (category_id)
+    REFERENCES theatre.production_category (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- Performance table
+DROP TABLE IF EXISTS theatre.performance ;
+
+CREATE TABLE IF NOT EXISTS theatre.performance (
+  id INT NOT NULL AUTO_INCREMENT,
+  performance_date DATE NOT NULL,
+  time_slot ENUM('matinee', 'evening') NOT NULL,
+  production_id INT NULL,
+  PRIMARY KEY (id),
+  INDEX production_id_idx (production_id ASC) ,
+  UNIQUE INDEX unique_index (performance_date ASC, time_slot ASC, production_id ASC) ,
+  CONSTRAINT FK_production_id
+    FOREIGN KEY (production_id)
+    REFERENCES theatre.production (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- Ticket table
+DROP TABLE IF EXISTS theatre.ticket ;
+
+CREATE TABLE IF NOT EXISTS theatre.ticket (
+  id INT NOT NULL AUTO_INCREMENT,
+  concession_id INT NOT NULL,
+  performance_id INT NOT NULL,
+  seat_id INT NOT NULL,
+  customer_id INT NOT NULL,
+  ticket_status ENUM('basket', 'purchased') NOT NULL,
+  sale_price Decimal (5,2) NOT NULL DEFAULT 99,
+  created TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  INDEX concession_id_idx (concession_id ASC) ,
+  INDEX FK_performance_id_idx (performance_id ASC) ,
+  INDEX FK_seat_id_idx (seat_id ASC) ,
+  INDEX FK_customer_id_idx (customer_id ASC) ,
+  UNIQUE INDEX ticket_unique_idx (performance_id ASC, seat_id ASC) ,
+  CONSTRAINT FK_concession_id
+    FOREIGN KEY (concession_id)
+    REFERENCES theatre.concession (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT FK_performance_id
+    FOREIGN KEY (performance_id)
+    REFERENCES theatre.performance (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT FK_seat_id
+    FOREIGN KEY (seat_id)
+    REFERENCES theatre.seat (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT FK_customer_id
+    FOREIGN KEY (customer_id)
+    REFERENCES theatre.customer (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- production_performers table
+DROP TABLE IF EXISTS theatre.production_performers ;
+
+CREATE TABLE IF NOT EXISTS theatre.production_performers (
+  id INT NOT NULL AUTO_INCREMENT,
+  production_id INT NOT NULL,
+  performer_id INT NOT NULL,
+  production_role VARCHAR(100) NULL,
+  PRIMARY KEY (id),
+  INDEX production_id_idx (production_id ASC) ,
+  INDEX perfomer_id_idx (performer_id ASC) ,
+  CONSTRAINT production_id
+    FOREIGN KEY (production_id)
+    REFERENCES theatre.production (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT perfomer_id
+    FOREIGN KEY (performer_id)
+    REFERENCES theatre.performer (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- Music_performers table
+DROP TABLE IF EXISTS theatre.music_performers ;
+
+CREATE TABLE IF NOT EXISTS theatre.music_performers (
+  id INT NOT NULL AUTO_INCREMENT,
+  production_id INT NOT NULL,
+  performer_id INT NOT NULL,
+  music_role VARCHAR(45) NULL,
+  PRIMARY KEY (id),
+  INDEX FK_music_performer_idx (performer_id ASC) ,
+  INDEX FK_production_id_idx (production_id ASC) ,
+  CONSTRAINT FK_music_performer
+    FOREIGN KEY (performer_id)
+    REFERENCES theatre.performer (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT FK_production
+    FOREIGN KEY (production_id)
+    REFERENCES theatre.production (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+;
+
+
+-- application_admin table
+DROP TABLE IF EXISTS theatre.application_admin;
+
+CREATE TABLE IF NOT EXISTS theatre.application_admin (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR(45) NOT NULL,
+  user_password VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id))
+;
+-- setup db user for Java connector
+drop user if exists 'theatre_user'@'localhost';
+create user 'theatre_user'@'localhost' identified by 'Pa$$word123';
+grant all privileges on theatre.* to 'theatre_user'@'localhost';
+flush privileges;
+
+-- add demo data
+
+-- set production categories.
+use theatre;
+insert into theatre.production_category (category_name)
+values
+('Opera'),
+('Concert'),
+('Musical'),
+('Theatre')
+;
+
+insert into theatre.production (title, production_description, duration, category_id, price, production_language)
+	values
+	('Oliver!','Oliver! - The Musical', 120, 3, 30, 'English'),
+	('The Mousetrap','Whodunit?', 150, 4, 30,'English'),
+	('Matilda','Adapted from Roald Dahl\'s novel', 150, 3, 35,'English'),
+    ('La bohème','Mimì and Rodolfo\'s passion burns with a brilliant flame. In a city of lights, can love eclipse Death itself?', 130, 1, 40, 'Italian')
+;
+
+insert into performer(performer_name)
+	values
+    ('Ron Moody'),
+    ('Georgia Brown'),
+    ('Danny Sewell'),
+    ('Keith Hamshire'),
+    ('Martin Horsey'),
+    
+    ('Karen Archer'),
+    ('Simon Haynes'),
+    ('Jamie Hutchins'),
+    ('Chrisopher Knott'),
+    ('Hannah Lee'),
+    ('Simon Roberts'),
+    ('Tom Rooke'),
+    ('Kirsten Hazel Smith'),
+    
+    ('Cleo Demetriou'),
+    ('Bertie Carvel'),
+    ('Lauren Ward'),
+    ('Paul Kaye'),
+    ('Josie Walker'),
+    
+    ('Evan Gorga'),
+    ('Cesira Ferrani'),
+    ('Tieste Wilmant'),
+    
+    ('La Scala Orchestra')
+    
+;
+
+insert into production_performers (production_id, performer_id, production_role)
+	values
+    (1,1, 'Fagin'),
+    (1,2, 'Nancy'),
+    (1,3, 'Bill Sykes'),
+    (1,4, 'Oliver Twist'),
+    (1,5, 'The Artful Dodger'),
+    
+    (2,6, 'Mrs Boyle'),
+    (2,7, 'Christopher Wren'),
+    (2,8, 'Detective Trotter'),
+    (2,9, 'Major Metcalf'),
+    (2,10, 'Miss Casewell'),
+    (2,11, 'Mr Paravicini'),
+    (2,12, 'Giles Ralston'),
+    (2,13, 'Mollie Ralston'),
+    
+    (3,14, 'Matilda Wormwood'),
+    (3,15, 'Miss Trunchbull'),
+    (3,16, 'Jennifer Honey'),
+    (3,17, 'Mr Wormwood'),
+    (3,18, 'Mrs Wormwood'),
+    
+    (4,19, 'Rodolfo'),
+    (4,20, 'Mimi'),
+    (4,21, 'Marcello')
+;
+
+insert into music_performers (production_id, performer_id, music_role)
+	values
+    (4,22, 'Orchestra')
+;
+
+
+/*
+insert into seat (id, location) 
+	values
+	(1,'stalls'),
+	(2,'stalls'),
+    (3,'stalls'),
+    (4,'stalls'),
+    (5,'circle'),
+    (6,'circle')
+    
+;
+*/
+
+DELIMITER //
+CREATE PROCEDURE bulk_stalls()
+BEGIN
+    DECLARE i int DEFAULT 1;
+    WHILE i <= 80 DO
+        INSERT INTO seat (id, location) VALUES (i, 'stalls');
+        SET i = i + 1;
+    END WHILE;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE bulk_circle()
+BEGIN
+    DECLARE i int DEFAULT 81;
+    WHILE i <= 200 DO
+        INSERT INTO seat (id, location) VALUES (i, 'circle');
+        SET i = i + 1;
+    END WHILE;
+END //
+DELIMITER ;
+CALL bulk_stalls();
+CALL bulk_circle();
+
+
+insert into customer (customer_name, customer_email, customer_password) value ('bob','bob@gmail','bob123');
+insert into concession (concession_name, discount)
+	values
+    ('Standard', 1),
+    ('Child', 0.75)
+;
+insert into performance( performance_date, time_slot, production_id)
+	values 
+    (date_add(curdate(), interval 1 day), 'matinee', 1),
+    (date_add(curdate(), interval 1 day), 'evening', 1),
+    
+	(date_add(curdate(), interval 2 day), 'matinee', 2),
+    (date_add(curdate(), interval 2 day), 'evening', 2),
+    
+	(date_add(curdate(), interval 3 day), 'matinee', 3),
+    (date_add(curdate(), interval 3 day), 'evening', 3),
+    
+	(date_add(curdate(), interval 4 day), 'matinee', 4),
+    (date_add(curdate(), interval 4 day), 'evening', 4)
+    
+;
+/*
+insert into ticket (concession_id, performance_id, seat_id, customer_id, ticket_status)
+	values 
+    (1,1,1,1,'basket'),
+    (1,1,2,1,'purchased')
+;
+*/
+
+-- add event
+-- show variables where variable_name='event_scheduler';
+use theatre;
+drop event if exists every_5_mins_remove_basket_tickets;
+delimiter $$
+
+create event every_5_mins_remove_basket_tickets
+	on schedule every 5 minute starts now()
+    on completion preserve
+do begin
+	delete from ticket
+    where timestampdiff(minute, created, now())>5 AND ticket_status = 'basket' ;
+end$$
+delimiter ;
